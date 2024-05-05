4363082233e031aa6b2414515b8f96888cb619a7
Santiago Mattiauda <santimattius@gmail.com>
2024-05-05 16:53:24 -0300

    [update]. update proyect

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,12 @@ pluginManagement {
         mavenCentral()
         google()
         gradlePluginPortal()
+        maven("https://www.jetbrains.com/intellij-repository/releases")
         maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.1.0")
+    id("org.jetbrains.amper.settings.plugin").version("0.2.0")
 }
